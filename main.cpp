5115031d879c0eb31aae95aee13da860fb5d897a
Leep <1732139001@qq.com>
2023-08-27 23:16:12 +0800

    添加线程池

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Thread/ThreadPool.h"
+#include "Serialize/DataStream.h"
 int main() {
 
     ThreadPool threadPool(4); // 创建一个拥有4个线程的线程池
@@ -12,5 +13,21 @@ int main() {
     // 等待所有任务完成
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
+    std::cout <<sizeof(bool)<<"###"<<sizeof(char)<<"\n";
+
+    DataStream ds;
+    map<int,int> map1;
+    for(int i = 0; i < 10; i++)
+    {
+        map1[i] = i+100;
+    }
+    ds.write(map1);
+
+    map<int,int> map2;
+    ds>>map2;
+    for(auto iter : map2)
+    {
+        cout << iter.first <<":"<<iter.second<<std::endl;
+    }
     return 0;
 }
